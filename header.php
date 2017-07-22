7fa8a8fdacd058c4724e46c906f22a4ccd092f84
acao <richard.j.schulte@gmail.com>
2017-07-21 21:00:06 -0400

    initialize the theme

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="https://fonts.googleapis.com/css?family=Droid+Sans:400,700" rel="stylesheet">
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+		<?php wp_head(); ?>
+	</head>
+	<body>
